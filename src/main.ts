1f123549cc505b60cb98ba4398f49216e5677cfa
Erik <erik.poromaa@gmail.com>
2019-10-07 10:02:02 +0200

    feat(main.tf): listening to two ports

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as http from 'http';
+import {ExpressAdapter} from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule);
+  // await app.listen(3000);
+  
+  const server = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+  await app.init();
+  
+  http.createServer(server).listen(3000);
+  http.createServer(server).listen(443);
 }
 bootstrap();
