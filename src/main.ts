5640173aac80232f752b9494c8e884387f8bc81f
Erik <erik.poromaa@gmail.com>
2019-10-07 10:26:04 +0200

    feat(main.ts): swagger docs for backoffice

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as express from 'express';
 import * as http from 'http';
 import {ExpressAdapter} from '@nestjs/platform-express';
 import { BackOfficeModule } from './modules/backoffice/backoffice.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
@@ -21,6 +22,16 @@ async function bootstrap() {
     BackOfficeModule,
     new ExpressAdapter(backOfficeServer),
   );
+  
+
+  const options = new DocumentBuilder()
+    .setTitle('BackOffice')
+    .setDescription('The back office API description')
+    .setVersion('1.0')
+    .addTag('backOffice')
+    .build();
+  const document = SwaggerModule.createDocument(backOfficeApp, options);
+  SwaggerModule.setup('api', backOfficeApp, document);
   await backOfficeApp.init();
 
   http.createServer(clientServer).listen(3000); // The public port (Load balancer will route traffic to this port)
