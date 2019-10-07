ddc34a45e9e3ad9023137f537eea2aad4e5d17a4
Erik <erik.poromaa@gmail.com>
2019-10-07 10:31:45 +0200

    feat(main.ts): seperate swagger docs for client & backoffice

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,20 @@ import { BackOfficeModule } from './modules/backoffice/backoffice.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // await app.listen(3000);
 
   const clientServer = express();
   const clientApp = await NestFactory.create(
     ClientModule,
     new ExpressAdapter(clientServer),
   );
+  const clientOptions = new DocumentBuilder()
+    .setTitle('ClientServer')
+    .setDescription('The client server API description')
+    .setVersion('1.0')
+    .addTag('client')
+    .build();
+  const clientDocument = SwaggerModule.createDocument(clientApp, clientOptions);
+  SwaggerModule.setup('api', clientApp, clientDocument);
   await clientApp.init();
 
   const backOfficeServer = express();
@@ -22,19 +28,18 @@ async function bootstrap() {
     BackOfficeModule,
     new ExpressAdapter(backOfficeServer),
   );
-  
 
-  const options = new DocumentBuilder()
+  const backOfficeOptions = new DocumentBuilder()
     .setTitle('BackOffice')
     .setDescription('The back office API description')
     .setVersion('1.0')
     .addTag('backOffice')
     .build();
-  const document = SwaggerModule.createDocument(backOfficeApp, options);
-  SwaggerModule.setup('api', backOfficeApp, document);
+  const backOfficeDocument = SwaggerModule.createDocument(backOfficeApp, backOfficeOptions);
+  SwaggerModule.setup('api', backOfficeApp, backOfficeDocument);
   await backOfficeApp.init();
 
   http.createServer(clientServer).listen(3000); // The public port (Load balancer will route traffic to this port)
-  http.createServer(backOfficeServer).listen(4000); // A port that will not be exposed public (Will be accessed through a bastian host or similar)
+  http.createServer(backOfficeServer).listen(4000); // The private port (Will be accessed through a bastian host or similar)
 }
 bootstrap();
